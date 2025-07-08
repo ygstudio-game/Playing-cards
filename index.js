c7e9408f85de38ea137f04ac5d2f658279e2e47b
ygstudio-game <ygstudio274@gmail.com>
2025-07-08 18:23:47 +0530

    online test

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     // origin: process.env.CLIENT_URL || 'http://localhost:3000',
-    origin: process.env.CLIENT_URL || 'https://playing-cards-tau.vercel.app/',
+    origin:   'https://playing-cards-tau.vercel.app/',
     methods: ['GET', 'POST']
   }
 });
